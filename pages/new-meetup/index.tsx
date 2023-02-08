4126e6a06502ff3ed3bf0fddd515e117eab86783
Frank Baus <frank.baus@ergosign.de>
2023-02-08 13:14:09 +0100

    removed api

--- pages/new-meetup/index.tsx
+++ pages/new-meetup/index.tsx
@@ -6,13 +6,13 @@ export default function NewMeetup() {
   const router = useRouter();
 
   async function onAddMeetup(meetup: Meetup) {
-    const response = await fetch('/api/new-meetup', {
+    /*const response = await fetch('/api/new-meetup', {
       method: 'POST',
       body: JSON.stringify(meetup),
       headers: {
         'Content-Type': 'application/json',
       },
-    });
+    });*/
 
     router.push('/');
   }
